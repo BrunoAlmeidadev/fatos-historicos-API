f4736f82d2443a78129e5c1628ac06f2aa94b981
Bruno <brunoritchieledev@gmail.com>
2023-11-08 18:11:11 -0300

    one

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { servicoBuscarFatoPorAno, servicoValidaAno } = require('./servico');
+const app = express();
+
+app.get('/', (req, res)  => {
+    let anoFato = req.query.ano;
+    if(servicoValidaAno(anoFato))
+    {
+        let fato = servicoBuscarFatoPorAno(anoFato);
+        res.json({fato : fato});
+    }
+    else
+    {
+        res.status(400).json({erro:'Parâmetro ano inválido'});
+    }
+});
+   
+
+app.listen(8080, () => {
+    console.log("Servidor rodando na porta: 8080");
+});
